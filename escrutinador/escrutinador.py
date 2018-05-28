8c2cdf73fffd86a6db738e7b4f846d55b318053f
Mateo Guty <mateogutigomez@gmail.com>
2018-05-28 09:29:47 -0500

    Adding empty escrutinador structure

---
+++ escrutinador/escrutinador.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+def publicDecipher():
+    pass
+
+def nonce():
+    pass
+
+def calcXor():
+    pass
+
+def privateDecipher():
+    pass
+
+def voteCount():
+    pass
+
+def main():
+     publicDecipher()
+     nonce()
+     calcXor()
+     privateDecipher()
+     voteCount()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+# USAR ISO-8859-1 PARA LOS JSON
