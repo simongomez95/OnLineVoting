3c4fda8a631e79ced1711316b9eeb62ffa8a2825
simongomez95 <simongomez95@gmail.com>
2018-06-12 19:38:57 -0500

    ffff

--- escrutinador/escrutinador.py
+++ escrutinador/escrutinador.py
@@ -75,4 +75,4 @@ def show(candidato):
     return str(votos[int(candidato)])
     
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=int("80"), debug=True)
